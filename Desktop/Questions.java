6a00687219ab3c1b7b774ca26422942af71a4147
Ayan Miram <ayanmiram2001@gmail.com>
2022-11-30 20:24:07 +0600

    ayan miram

---
+++ Desktop/Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: AYAN");
+        System.out.println("Surname: MIRAM");
+        System.out.println("Id: 180107027");
+        System.out.println("Hobby: Drinking");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 11");
+        System.out.println("Am I the best teacher?: YEs of course!!!!!!!!!!!!");
+    }
+}
